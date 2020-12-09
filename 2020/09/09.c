2819783c8a41bad093efda90aef786292a324627
Thinklex <alexl.id.at@gmail.com>
2020-12-09 07:23:06 +0100

    [09 fin]

--- 2020/09/09.c
+++ 2020/09/09.c
@@ -3,101 +3,183 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <limits.h>
+#include <string.h>
 
-int main(int argc, char * argv[]) {
+int main (int argc, char *argv[])
+{
 
-    if(argc != 3) {
-       fprintf(stderr,"[%s]: Error: Usage %s -i <FILE>",argv[0], argv[0]);
-       return EXIT_FAILURE;
+    //Input validation
 
-    }
+    if (argc != 4)
+	{
+	    fprintf (stderr,
+		     "[%s]: Error: Usage %s -i <FILE> <NumberOfLinesForCalculation\n",
+		     argv[0], argv[0]);
+	    return EXIT_FAILURE;
+	}
 
-    char* fileName = argv[2];
+    int opt;
+    char *fileName = NULL;
+
+    while ((opt = getopt (argc, argv, "i:")) != -1)
+	{
+	    switch (opt)
+		{
+		case 'i':
+		    fileName = optarg;
+		    break;
+		default:
+		    fprintf (stderr,
+			     "[%s]: Usage: %s -i <FILE> <NumberOfLinesForCalculation>\n",
+			     argv[0], argv[0]);
+		    return EXIT_FAILURE;
+		}
+	}
+
+    int indexPosArg = optind;
+    int numbersConsidered = -1;
+    char *errBuf = NULL;
+
+    if (argv[indexPosArg] != NULL)
+	{
+	    numbersConsidered = strtol (argv[indexPosArg], &errBuf, 10);
+	}
+
+    if (fileName == NULL || numbersConsidered < 1 || strlen (errBuf) > 0)
+	{
+	    fprintf (stderr,
+		     "[%s]: Something wrong with the option -i or one argument given.\n",
+		     argv[0]);
+	    return EXIT_FAILURE;
+	}
+
+    //-----------------READ_FROM_FILE-----------------------------------------------------
 
     char buf;
     ssize_t readBytes;
 
-    int fd = open(fileName,O_RDONLY);
+    int fd = open (fileName, O_RDONLY);
 
-    if(fd == -1) {
-        fprintf(stderr,"[%s]: Error opening file: %s", argv[0], fileName);
-        return EXIT_FAILURE;
-    }
+    if (fd == -1)
+	{
+	    fprintf (stderr, "[%s]: Error opening file: %s", argv[0],
+		     fileName);
+	    return EXIT_FAILURE;
+	}
 
 
-    int listSize = 1001;
-    long * preamble = malloc(sizeof(long) * listSize);
-    int curPos = 0;
+    int listSize = 128;
+    long *preamble = malloc (sizeof (long) * listSize);
+    int inputSize = 0;
 
     long curNum = 0;
-    while((readBytes = read(fd,&buf,1)) > 0) {
+    while ((readBytes = read (fd, &buf, 1)) > 0)
+	{
 
-        if(buf >= '0' && buf <= '9') {
-            int bufAsInt = buf - '0';
-            curNum = (curNum * 10) + bufAsInt;
-        } else if(buf == '\n') {
-            preamble[curPos] = curNum;
-            curNum = 0;
-            curPos++;
-        } else {
-            fprintf(stderr,"<<OTHER_CHAR: %c>>\n",buf);
-        }
+	    if (buf >= '0' && buf <= '9')
+		{
+		    int bufAsInt = buf - '0';
+		    curNum = (curNum * 10) + bufAsInt;
+		}
+	    else if (buf == '\n')
+		{
+		    preamble[inputSize] = curNum;
+		    curNum = 0;
+		    inputSize++;
+		}
+	    else
+		{
+		    fprintf (stderr, "<<OTHER_CHAR: %c>>\n", buf);
+		}
 
+	    if (inputSize >= (listSize - 1))
+		{
+		    int newListSize = listSize * 2;
+		    long *preamble2 = malloc (sizeof (long) * newListSize);
 
-    }
+		    for (int i = 0; i < inputSize; i++)
+			{
+			    preamble2[i] = preamble[i];
+			}
 
-    int numbersConsidered = 25;
+		    free (preamble);
+
+		    preamble = preamble2;
+		    listSize = newListSize;
+		}
+	}
+
+    //------------GET_INVALID_NUM------------------------------------
+
     int invalidNum = -1;
 
-    for(int i = 0; i < curPos; i++) {
-        if(i >= numbersConsidered) {
-            int start = i - numbersConsidered;
-            int end = i - 1;
+    for (int i = 0; i < inputSize; i++)
+	{
+	    if (i >= numbersConsidered)
+		{
+		    int start = i - numbersConsidered;
+		    int end = i - 1;
 
-            int doesItSumUp = 0;
-            for(int j = start; j <= end; j++) {
-                for(int k = j + 1; k <= end; k++) {
-                    if((preamble[j] + preamble[k]) == preamble[i]) {
-                        doesItSumUp = 1;
-                        //fprintf(stdout,"<%ld + %ld = %ld || Line num: <%d + %d = %d>>\n",preamble[j], preamble[k], preamble[i], j, k, i);
-                    }
-                }
-            }
+		    int doesItSumUp = 0;
+		    for (int j = start; j <= end; j++)
+			{
+			    for (int k = j + 1; k <= end; k++)
+				{
+				    if ((preamble[j] + preamble[k]) ==
+					preamble[i])
+					{
+					    doesItSumUp = 1;
+					}
+				}
+			}
 
-            if(doesItSumUp == 0) {
-                fprintf(stdout,">! %ld line num %d!<\n", preamble[i], i);
-                invalidNum = preamble[i];
-            }
-        }
-    }
+		    if (doesItSumUp == 0)
+			{
+			    fprintf (stdout, ">! %ld line num %d!<\n",
+				     preamble[i], i);
+			    invalidNum = preamble[i];
+			}
+		}
+	}
 
+    //---------------------CALC_SUB_SUM------------------------------------
 
-    for(int i = 0; i < curPos; i++) {
-        
-        int curIndex = i;
-        long curSum = 0;
+    for (int i = 0; i < inputSize; i++)
+	{
 
-        long min = LONG_MAX;
-        long max = LONG_MIN;
+	    int curIndex = i;
+	    long curSum = 0;
 
-        while(curSum < invalidNum) {
-            if(preamble[curIndex] < min) {
-                min = preamble[curIndex];
-            }
-            if(preamble[curIndex] > max) {
-                max = preamble[curIndex];
-            }
-            curSum = curSum + preamble[curIndex];
-            curIndex++;
-    
-        }
-        curIndex--;
+	    long min = LONG_MAX;
+	    long max = LONG_MIN;
 
-        if(curSum == invalidNum && (i != curIndex)) { 
-            fprintf(stdout,">!!%ld -> %ld -> %ld || %d -> %d !!<\n",preamble[i],preamble[curIndex],(preamble[i]+preamble[curIndex]), i, curIndex);
-            fprintf(stdout,">!!MIN: %ld MAX: %ld SUM: %ld <!!\n",min,max,(min+max));
-        }
-    }
+	    while (curSum < invalidNum)
+		{
+		    if (preamble[curIndex] < min)
+			{
+			    min = preamble[curIndex];
+			}
+		    if (preamble[curIndex] > max)
+			{
+			    max = preamble[curIndex];
+			}
+		    curSum = curSum + preamble[curIndex];
+		    curIndex++;
+
+		}
+	    curIndex--;
+
+	    if (curSum == invalidNum && (i != curIndex))
+		{
+		    fprintf (stdout, ">!!%ld -> %ld -> %ld || %d -> %d !!<\n",
+			     preamble[i], preamble[curIndex],
+			     (preamble[i] + preamble[curIndex]), i, curIndex);
+		    fprintf (stdout, ">!!MIN: %ld MAX: %ld SUM: %ld <!!\n",
+			     min, max, (min + max));
+		}
+	}
+
+    free (preamble);
 
     return EXIT_SUCCESS;
 }
